1f3e60d21bc2a2d3278844b25ce7f0c70c2608bb
Wakaru Himura <wakaru44@gmail.com>
2016-04-24 19:16:45 +0200

    idempotence fix for install

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 echo "Installing daemonize"
-git clone git://github.com/bmc/daemonize.git
-cd daemonize
-sh configure
-make
-sudo make install 
-cd ..
+if [ ! -f daemonize/daemonize ]; then
+    git clone git://github.com/bmc/daemonize.git
+    cd daemonize
+    sh configure
+    make
+    sudo make install 
+    cd ..
+fi
 
 echo "Building the app"
 go build hiworld.go
