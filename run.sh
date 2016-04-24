056c68e991773a9aab419c63a10c853e43008d38
juanantoniofm <juanantoniofm.11@gmail.com>
2016-04-24 22:18:07 +0200

    added note on interruption

--- run.sh
+++ run.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-echo "Use daemonize to run the app"
+echo "daemonize to run the app"
 
 WORKPATH=$(pwd)
 
 if [ -f /var/run/goapp ]
 then
-
         PID=$(cat /var/run/goapp)
-
         if ps -p $PID >/dev/null
         then
             echo "The app is already running"
             ps -ef | grep -v grep | grep hiworld
+        else
+            echo "The app was interrupted in a previous run. Please clean your environment"
         fi
-
 else
-        daemonize/daemonize -a -p /var/run/goapp -l lock -u nobody $WORKPATH/hiworld
+    echo "Starting hiworld app"
+    daemonize/daemonize -a -p /var/run/goapp -l lock -u nobody $WORKPATH/hiworld
+fi
 
